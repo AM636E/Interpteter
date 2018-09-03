9948fbad85e7c4c9bdbac8edd647d3a4c14d9b8b
Nazarii Brenchuk <nazarii@leventprojects.com>
2018-09-03 15:09:37 +0300

    Added UIs

--- Calculator.Tests/InterpreterTests.cs
+++ Calculator.Tests/InterpreterTests.cs
@@ -13,6 +13,8 @@ namespace Calculator.Tests
         [InlineData("2-2", 0)]
         [InlineData("2^2", 4)]
         [InlineData("2^2^3", 256)]
+        [InlineData("2^(1+1)^(2+1)", 256)]
+        [InlineData("2^(1*2)^(1.5*2)", 256)]
         [InlineData("1.5*2", 3)]
         public void TestExpressions(string expression, double expected)
         {
---
+++ Calculator.Tests/ParserTests.cs
@@ -0,0 +1,30 @@
+﻿using Calculator.Nodes;
+using System;
+using Xunit;
+
+namespace Calculator.Tests
+{
+    public class ParserTests
+    {
+        [Fact]
+        public void TestSimple()
+        {
+            var parser = new Parser("2+2");
+            var tree = parser.Parse();
+
+            Assert.IsType<BinaryNode>(tree);
+
+            var binaryNode = tree as BinaryNode;
+
+            Assert.IsType<NumberNode>(binaryNode.Left);
+            Assert.Equal(TokenType.Plus, binaryNode.Op.Type);
+            Assert.IsType<NumberNode>(binaryNode.Right);
+
+            var left = binaryNode.Left as NumberNode;
+            var right = binaryNode.Right as NumberNode;
+
+            Assert.Equal(2, left.Value);
+            Assert.Equal(2, right.Value);
+        }
+    }
+}
